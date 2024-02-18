4e86daf8a4bd1abec16e8b262d89b2a451367b4f
Sheen <118210155+sheen123atti@users.noreply.github.com>
2024-02-18 18:44:48 +0530

    Update si.js

--- si.js
+++ si.js
@@ -1,8 +1,8 @@
 const express = require('express');
-// const cors = require("cors");
+const cors = require("cors");
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.get('/si', function(req, res){
