2e22fc0f18d4c777a78a942f57f54685765ce6e4
Sheen <118210155+sheen123atti@users.noreply.github.com>
2024-02-18 18:11:25 +0530

    Add files via upload

---
+++ si.js
@@ -0,0 +1,21 @@
+const express = require('express');
+// const cors = require("cors");
+const app = express();
+
+// app.use(cors());
+app.use(express.json());
+
+app.get('/si', function(req, res){
+    const p = parseInt(req.query.p);
+    const r = parseInt(req.query.r);
+    const t = parseInt(req.query.t);
+
+    const i = (p*r*t)/100;
+    const a = p+i;
+    res.send({
+        'interest' : i,
+        'amount' : a
+    })
+});
+
+app.listen(3003);
